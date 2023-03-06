1940a7f469ddd589ac29d94f4110c569d390bd9f
DrManganese <music.mathias@gmail.com>
2023-03-06 22:04:09 +0100

    Added mermaid rendering

---
+++ themes/mathiasdejongbe/static/mermaid_theme.js
@@ -0,0 +1,9 @@
+const themeVariables = {
+        primaryColor: "hsl(219, 70%, 96%)",
+        primaryTextColor: "hsl(0, 0%, 21%)",
+        primaryBorderColor:  "hsl(217, 71%, 53%)",
+        lineColor: "hsl(0, 0%, 48%)",
+        secondaryColor: "hsl(0, 0%, 96%)",
+        clusterBkg: "hsl(48, 100%, 96%)",
+        clusterBorer: "hsl(48, 100%, 67%)"
+      };
